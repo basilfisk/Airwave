c23edb51e708fa8c7a4a677e0bbdbb0f9a3134cb
“Basil <bf@breato.com>
2016-03-15 11:43:14 +0000

    Complete rewrite to fix bugs that did not unwrap the JSON or XML documents properly from the API response message

--- bin/portal/metadata.pl
+++ bin/portal/metadata.pl
@@ -22,6 +22,7 @@ use warnings;
 # System modeles
 use Data::Dumper;
 use Getopt::Long;
+use JSON::XS;
 
 # Breato modules
 use lib "$ROOT";
@@ -30,7 +31,7 @@ use mods::Common qw(logMsg logMsgPortal readConfig writeFile);
 
 # Program information
 our $PROGRAM = "metadata.pl";
-our $VERSION = "1.0";
+our $VERSION = "2.1";
 
 # Check there are any arguments
 if($#ARGV == -1) { usage(); }
@@ -65,36 +66,100 @@ main();
 # ---------------------------------------------------------------------------------------------
 sub main {
 	my($text);
-	my $dir = "../$CONFIG{PORTAL_META}/$PROVIDER/$FILM";
 	
 	# Start up message
 	logMsg($LOG,$PROGRAM,"=================================================================================");
-	logMsg($LOG,$PROGRAM,"Generating metadata for: $FILM");
 
 	# Read the JSON metadata from the Portal and create a file
-	$text = apiMetadata('apMetadata',$FILM,'json');
-	if(!$text) {
-		logMsgPortal($LOG,$PROGRAM,'E',"Prepare: Could not read JSON metadata from the Portal: [code] text");
-		return;
-	}
-	if(writeFile("$ROOT/$dir/$FILM.json",$text)) {
-		logMsg($LOG,$PROGRAM,"JSON metadata written to file: $dir/$FILM.json");
-	}
-	else {
-		logMsgPortal($LOG,$PROGRAM,'E',"Prepare: Could not write JSON metadata to file: $dir/$FILM.json");
-	}
+	$text = read_metadata('json');
+	if(!$text) { return; }
+	if(!write_metadata('json',$text)) { return; }
 	
 	# Read the XML metadata from the Portal and create a file
-	$text = apiMetadata('apMetadata',$FILM,'xml');
-	if(!$text) {
-		logMsgPortal($LOG,$PROGRAM,'E',"Prepare: Could not read XML metadata from the Portal: [code] text");
+	$text = read_metadata('xml');
+	if(!$text) { return; }
+	if(!write_metadata('xml',$text)) { return; }
+}
+
+
+
+# ---------------------------------------------------------------------------------------------
+# Convert a string in JSON format to a hash
+#
+# Argument 1 : String in JSON format
+#
+# Return (pointer,undef) to a hash of data if successful, or (undef,message) if errors
+# ---------------------------------------------------------------------------------------------
+sub json_data {
+	my($string) = @_;
+	my($hash_ref);
+
+	# Parse the string and trap any errors
+	eval { $hash_ref = JSON::XS->new->latin1->decode($string) or die "error" };
+	if($@) {
+		return (undef,$@);
+	}
+	return ($hash_ref,undef);
+}
+
+
+
+# ---------------------------------------------------------------------------------------------
+# Read metadata from the Portal
+#
+# Argument 1 : Type of content (json|xml)
+#
+# Return metadata for success or undef for error
+# ---------------------------------------------------------------------------------------------
+sub read_metadata {
+	my($type) = @_;
+	my($msg,$status,%error,$ref,$msg,%meta);
+	my $name = uc($type);
+	logMsg($LOG,$PROGRAM,"Generating $name metadata for $FILM");
+	
+	$msg = apiMetadata('apMetadata',$FILM,$type);
+	($status,%error) = apiStatus($msg);
+	if(!$status) {
+		logMsgPortal($LOG,$PROGRAM,'E',"Prepare: Could not read $name metadata from Portal [$error{CODE}] $error{MESSAGE}");
 		return;
 	}
-	if(writeFile("$ROOT/$dir/$FILM.xml",$text)) {
-		logMsg($LOG,$PROGRAM,"XML metadata written to file: $dir/$FILM.xml");
+	else {
+		# Return metadata from response message
+		%meta = apiData($msg);
+		if ($type eq 'json') {
+			return encode_json(\%meta);
+		}
+		else {
+			$msg = $meta{'xml'};
+			$msg =~ s/&quot;/"/g;
+			return $msg
+		}
+	}
+}
+
+
+
+# ---------------------------------------------------------------------------------------------
+# Read metadata from the Portal
+#
+# Argument 1 : Type of content (json|xml)
+# Argument 2 : Metadata text
+#
+# Return 1 for success or 0 for error
+# ---------------------------------------------------------------------------------------------
+sub write_metadata {
+	my($type,$text) = @_;
+	my $dir = "../$CONFIG{PORTAL_META}/$PROVIDER/$FILM";
+	my $name = uc($type);
+	logMsg($LOG,$PROGRAM,"Writing $name metadata to Portal for $FILM");
+	
+	if(writeFile("$ROOT/$dir/$FILM.$type",$text)) {
+		logMsg($LOG,$PROGRAM,"$name metadata written to file: $dir/$FILM.$type");
+		return 1;
 	}
 	else {
-		logMsgPortal($LOG,$PROGRAM,'E',"Prepare: Could not write XML metadata to file: $dir/$FILM.xml");
+		logMsgPortal($LOG,$PROGRAM,'E',"Prepare: Could not write $name metadata to file: $dir/$FILM.$type");
+		return 0;
 	}
 }
 

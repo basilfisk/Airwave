4ae937e4802ed321661ae204d50d45f09c5db075
“Basil <bf@breato.com>
2016-08-12 14:04:32 +0100

    Switched to API v3

--- bin/monitor.pl
+++ bin/monitor.pl
@@ -25,7 +25,7 @@ use Getopt::Long;
 
 # Breato modules
 use lib "$ROOT";
-use mods::API qw(apiDML apiData apiStatus);
+use mods::API3Distro qw(apiDML apiData apiStatus);
 use mods::Common qw(cleanString formatDateTime logMsg logMsgPortal parseDocument readConfig);
 
 # Program information
@@ -73,18 +73,18 @@ main();
 sub main {
 	my(@net,@svr);
 	my $cds = 0;
-	
+
 	# Read network statistics
 	@net = network_stats();
-	
+
 	# Read server statistics
 	@svr = server_stats();
-	
+
 	# Read CDS distribution statistics
 	if($HOSTNAME eq 'distro') {
 		$cds = cds_stats();
 	}
-	
+
 	# Log stats
 	if(@svr) {
 		log_stats(@svr,$cds,@net);
@@ -100,7 +100,7 @@ sub main {
 # ---------------------------------------------------------------------------------------------
 sub cds_stats {
 	my($sql,$dsn,$dbh,$sth,$ref,$id,%data);
-	
+
 	# Running distributions
 	$sql = <<SQL_RUNNING;
 SELECT	d.DistributionID AS DistID,
@@ -108,31 +108,31 @@ SELECT	d.DistributionID AS DistID,
 		d.Timestamp AS Start,
 		(SELECT MAX(s.Name) FROM Action a,Server s WHERE a.DestServerID = s.ServerID AND a.DistributionID=d.DistributionID) AS Site,
 		(SELECT COUNT(*) FROM Error e WHERE e.DistributionID=d.DistributionID) AS Errors,
-		(SELECT SUM(f.Size*a.Progress)/SUM(f.Size) FROM Action a LEFT JOIN File f ON (a.ObjectID=f.FileID) WHERE a.DistributionID=d.DistributionID AND a.EventTypeID=(SELECT DISTINCT EventTypeID FROM EventType WHERE Name='transfer')) AS Progress 
+		(SELECT SUM(f.Size*a.Progress)/SUM(f.Size) FROM Action a LEFT JOIN File f ON (a.ObjectID=f.FileID) WHERE a.DistributionID=d.DistributionID AND a.EventTypeID=(SELECT DISTINCT EventTypeID FROM EventType WHERE Name='transfer')) AS Progress
 FROM	Distribution d,
-		Content c 
-WHERE	d.ContentID = c.ContentID 
+		Content c
+WHERE	d.ContentID = c.ContentID
 AND		0<(
-			SELECT	COUNT(*) 
-			FROM	Action a 
-			WHERE	a.DistributionID=d.DistributionID 
-			AND		a.Status='ready' 
+			SELECT	COUNT(*)
+			FROM	Action a
+			WHERE	a.DistributionID=d.DistributionID
+			AND		a.Status='ready'
 			AND		a.EventTypeID IN (SELECT DISTINCT EventTypeID FROM EventType WHERE Name IN ('transfer','ingest'))
 		)
 SQL_RUNNING
-	
+
 	# Connect to the database
 	$dsn = "DBI:mysql:database=$CONFIG{CDS_SQL_DATABASE};host=$CONFIG{CDS_SQL_HOST};mysql_connect_timeout=5";
 	eval {
 		$dbh = DBI->connect($dsn,$CONFIG{CDS_SQL_USERNAME},$CONFIG{CDS_SQL_PASSWORD},{RaiseError => 1});
 	};
-	
+
 	# Error raised during connection
 	if ($@) {
 		logMsgPortal($LOG,$PROGRAM,'E',"Can't connect to CDS Portal: ".$@);
 		return 0;
 	}
-	
+
 	# Run the query and load the data into a hash
 	$sth = $dbh->prepare($sql);
 	$sth->execute();
@@ -147,10 +147,10 @@ SQL_RUNNING
 		}
 	}
 	$sth->finish();
-	
+
 	# Disconnect from the database
 	$dbh->disconnect();
-	
+
 	# Return number of CDS processes
 	return keys(%data);
 }
@@ -175,22 +175,22 @@ SQL_RUNNING
 sub log_stats {
 	my($time,$load1,$load5,$load15,$cpu,$cpupct,$mempct,$cds,$recv,$sent) = @_;
 	my($date,$fh,$status,$msg,%error);
-	
+
 	# Today's date
 	$date = formatDateTime('ccyy-zm-zd');
-	
+
 	# Write stats to log file and append date as 1st field
 	if(open($fh,">>$STATFILE")) {
 		print $fh "$date $time $load1 $load5 $load15 $cpu $cpupct $mempct $cds $recv $sent\n";
 		close($fh);
 	}
-	
+
 	# Make some changes as needed
 	$time = "$date $time";
 	$load1 = 100*$load1;
 	$load5 = 100*$load5;
 	$load15 = 100*$load15;
-	
+
 	# Write stats to Portal
 	($msg) = apiDML('monitorAddStats',"timestamp=$time","load1=$load1","load5=$load5","load15=$load15","cpu=$cpu","cpupct=$cpupct","memorypct=$mempct","cdsprocs=$cds","nwrecv=$recv","nwsent=$sent","server=$HOSTNAME");
 	($status,%error) = apiStatus($msg);
@@ -218,7 +218,7 @@ sub log_stats {
 sub network_check {
 	my($res,@data,@row);
 	my @net = (0,0);
-	
+
 	# Read the network statistics
 	$res = `cat /proc/net/dev`;
 	@data = split(/\n/,$res);
@@ -247,12 +247,12 @@ sub network_check {
 sub network_stats {
 	my(@start,@end,$recv,$sent);
 	my $interval = 10;
-	
+
 	# Capture the bytes sent/received over a 10 second interval
 	@start = network_check();
 	sleep $interval;
 	@end = network_check();
-	
+
 	# Convert to bits/second
 	$recv = 8*($end[0]-$start[0])/$interval;
 	$sent = 8*($end[1]-$start[1])/$interval;
@@ -276,12 +276,12 @@ sub network_stats {
 sub server_stats {
 	my($pid,$res,@data,@stats,$ts);
 	my $cds = 1;
-	
+
 	# Read the Airship PID
 	$res = `ps -ef | grep -e 'airship\$'`;
 	@data = split(/ /,cleanString($res));
 	$pid = $data[1];
-	
+
 	# If Airship isn't running, use /sbin/init to get the CPU stats
 	if(!$pid) {
 		$cds = 0;
@@ -289,10 +289,10 @@ sub server_stats {
 		@data = split(/ /,cleanString($res));
 		$pid = $data[1];
 	}
-	
+
 	# Run top 5 times to get correct CPU data
 	$res = `top -p $pid -n 5 -b > $TEMPFILE`;
-	
+
 	# Read timestamp and 3 CPU load averages
 	# top - 13:28:37 up 23 days, 22:28,  2 users,  load average: 1.10, 0.84, 0.74
 	$res = `tail -10 $TEMPFILE | grep average`;
@@ -300,14 +300,14 @@ sub server_stats {
 	$data[-3] =~ s/,//;
 	$data[-2] =~ s/,//;
 	push(@stats,($data[2],$data[-3],$data[-2],$data[-1]));
-	
+
 	# Read time CPU has spent running users' processes
 	# Cpu(s):  1.7%us,  0.2%sy,  0.0%ni, 97.9%id,  0.1%wa,  0.0%hi,  0.0%si,  0.0%st
 	$res = `tail -10 $TEMPFILE | grep Cpu`;
 	@data = split(/ /,cleanString($res));
 	$data[1] =~ s/%us,//;
 	push(@stats,$data[1]);
-	
+
 	# Read CPU usage and memory usage
 	#   PID USER      PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND
 	#  1909 root      20   0  462m 124m 2792 S   92  1.5   1905:38 airship
@@ -319,7 +319,7 @@ sub server_stats {
 	else {
 		push(@stats,(0,0));
 	}
-	
+
 	# Return the stats
 	return @stats;
 }
@@ -332,7 +332,7 @@ sub server_stats {
 # ---------------------------------------------------------------------------------------------
 sub usage {
 	my($err) = @_;
-	
+
 	printf("
 Program : $PROGRAM
 Version : v$VERSION
@@ -343,12 +343,12 @@ Summary :
   currently running on the server.
 
 Usage   : $PROGRAM
-  
+
   OPTIONAL
     --log                  If set, the results from the script will be written to the Airwave
                            log directory, otherwise the results will be written to the screen.
 	\n");
-	
+
 	# Stop in all cases
 	exit;
 }

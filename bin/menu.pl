785c8cf8fd3cbea7e991cbe334467cc7dc0eb04f
“Basil <bf@breato.com>
2016-08-14 08:58:48 +0100

    Use the API3 module

--- bin/menu.pl
+++ bin/menu.pl
@@ -19,7 +19,7 @@ use warnings;
 
 # Breato modules
 use lib "$ROOT";
-use mods::API qw(apiData apiSelect apiStatus);
+use mods::API3 qw(apiData apiSelect apiStatus);
 use mods::Common qw(formatDateTime logMsg readConfig);
 use mods::TK;
 
